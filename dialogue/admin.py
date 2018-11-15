f19686303c8d12b1de4775b9b40b6ed77ca1851c
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-15 16:58:42 +0900

    管理画面に検索機能を追加

--- dialogue/admin.py
+++ dialogue/admin.py
@@ -4,14 +4,19 @@ from .models import Question, Answer, Keyword
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_id', 'question_text', 'parent_id', 'created_date', 'modified_date')
+    search_fields = ['question_text']
 
 
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('answer_id', 'question_id', 'answer_texts', 'created_date', 'modified_date')
+    list_filter = ['question_id__question_id']
+    search_fields = ['answer_texts']
 
 
 class KeywordAdmin(admin.ModelAdmin):
     list_display = ('keyword_id', 'answer_id', 'word', 'weight', 'created_date', 'modified_date')
+    list_filter = ['answer_id__answer_id']
+    search_fields = ['word']
 
 
 admin.site.register(Question, QuestionAdmin)
