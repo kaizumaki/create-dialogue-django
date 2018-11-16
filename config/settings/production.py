114745e09e362cd2b7f7443e716f4b28533b9cad
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-17 04:29:10 +0900

    STATIC_ROOTの設定を分ける

--- config/settings/production.py
+++ config/settings/production.py
@@ -53,3 +53,9 @@ DATABASES = {
         },
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = 'https://kaizumaki.net/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
