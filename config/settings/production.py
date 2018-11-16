7ea4cf29d4a96eb9de34e7044566f3f59c8e0a1c
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-17 07:11:31 +0900

    static修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -59,8 +59,5 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'https://kaizumaki.net/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    "/code/static",
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
