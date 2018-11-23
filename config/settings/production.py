f2db55788a3e541d5a34086ab830e67806ede90d
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-24 01:39:45 +0900

    メール送信まわりを修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -39,4 +39,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.kaizumaki.net'
-EMAIL_PORT = 25
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
