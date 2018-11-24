bbd2085fb96d80b549416fd0bcf587c699dfa6d0
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-25 07:29:15 +0900

    testserverをALLOWED_HOSTSに追加

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '../env/production/.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['kaizumaki.net']
+ALLOWED_HOSTS = ['kaizumaki.net', 'testserver']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
