02300742b57b39e8d96d911d64c117b640f6b375
Kazuki Imamura <kaizumaki@gmail.com>
2018-11-29 16:00:28 +0900

    不要なimportを削除

--- dialogue/views.py
+++ dialogue/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, generics
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
